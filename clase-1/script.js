8d63b55ffe69964af702abaac5b3333490474abf
Francisco Pugh <franciscopugh01@gmail.com>
2022-07-13 16:44:59 -0300

    Clase 1

---
+++ clase-1/script.js
@@ -0,0 +1,32 @@
+/*let nombre = "Francisco"; //String
+let apellido = 'Pugh'; //String
+let edad = 45; //Number
+let profesor = true; //Boolean o Logica
+let sueldo = null;
+let salgoElFinde = undefined;
+let salgo_a_correr;
+const IVA = 1.21
+
+IVA = 1.30
+//console.log(nombre)
+nombre = 123;
+console.log(nombre);
+nombre = true;
+console.log(nombre);
+nombre = null;
+console.log(nombre);
+*/
+                //Parseo de String a Number
+let numero1 = parseFloat(prompt("Ingrese un numero"));
+let numero2 = parseInt(prompt("Ingrese otro numero")); //5 como si fuera "5"
+
+alert(numero1)
+alert(numero2)
+
+/*
+    5 + 5 = 10
+    "5" + "5" = "55"
+    "5" + 5 = "55"
+*/
+
+console.log(numero1 + numero2)
